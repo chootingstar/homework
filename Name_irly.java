4dcb9f73d6d84f061bf5273d4f09f6c9aced132d
chootingstar <jmirayes96@gmail.com>
2018-12-19 19:08:41 -0800

    First hw repo commit

--- Name_irly.java
+++ Name_irly.java
@@ -1,4 +1,29 @@
+import java.util.Scanner;
 
 public class Name_irly {
+	public static void main(String args[]) {
+		
+		
+		System.out.println(namePrompt());
+	}
+	
+	
+	
+	
+	public static String namePrompt() {
+		Scanner userInput= new Scanner(System.in);
+		System.out.println("Give me your name!");
+		String theName = userInput.nextLine();
+		return theName;
+	}
+	
+	public static String upper(String theName) {
+		theName = theName.toUpperCase();
+		return theName;
+	}
+	
+	
+		
+		
 	
 }
