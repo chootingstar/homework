5b5c722c41b34d4a273f93522473481104a0e57a
chootingstar <jmirayes96@gmail.com>
2019-01-04 13:55:10 -0800

    Sales.java update

--- Sales.java
+++ Sales.java
@@ -1,41 +1,50 @@
 
 public class Sales {
+// public int[] salesFolk = new int[5];
+
 	
-	int[][] salesFolk;
-	salesFolk = new int[5][5];
+//	public int SalesFolk() {};
+	//int[] SalesFolk = new int[5];
+	
+	//Why does this not work?
+	static int[] sellers(int j) {
+	int[] salesFolk;
+	salesFolk = new int[5];
+	salesFolk [j];
+	return salesFolk;
+	}
 	
 	public static void main(String[] args) {
-		Sales sold;
-		sold = new Sales();
-		
+				
 		//I plan to say a lot of data
-		System.out.println(sold.setArray());
+		setArray();
+		System.out.println(sellers());
 		System.out.println();
 		System.out.println();
 	}
 	
 	//My plans require an array, but I don't want to hard code all those numbers
-	public int setArray() {
+	public static int setArray() {
 		Sales rn = new Sales();
 		int rd = 0;
 		for (int i=0; i<5; i++) {
-			for (int j=0; j<5; j++) {
+			
 				rd = rn.randGen();
 				//I need the generator to be inside of this loop so that it generates new numbers
-				salesFolk [i][j] = rd;
+				sellers(rd);
 				/*I figured that the reason I couldn't call the random number generator was because I kept
 				 * trying to initially set the number, a start you can't do twice. But now the only thing I
 				 * think is the problem is that I can't initialize the number setting in an array. Apparently
 				 * array constants can only be applied at initialization. I don't how to get this to think of it
 				 * as the initial setting.*/
-			}
+			
 		}
 		
 	}
 	
-	public int func2() {
+	//public int func2() {
 		
-	}
+	//}
 	
 	int randGen() {
 		//taken from my guessing game program
